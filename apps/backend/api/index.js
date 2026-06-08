1c09f3953eff13f47d1c6a4b3626847f7ca2051a
Shivam mishra <shivammishra993187@gmail.com>
2026-06-09 00:59:25 +0530

    MVP Working now

--- apps/backend/api/index.js
+++ apps/backend/api/index.js
@@ -24,7 +24,7 @@ app.get("/api/health", (_, res) => res.json({ status: "ok" }));
 
 // Local dev server
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 4000;
   app.listen(PORT, () =>
     console.log(`Backend running at http://localhost:${PORT}`)
   );
